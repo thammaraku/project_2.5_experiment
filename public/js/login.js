2ea1190fd7825fbbbc430111a5a7c323d654ef5c
thamma <thammaraku@gmail.com>
2021-01-26 10:35:34 -0600

    fix eslint

--- public/js/login.js
+++ public/js/login.js
@@ -40,9 +40,8 @@ $(document).ready(() => {
   }
 });
 
-
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
+  const profile = googleUser.getBasicProfile();
   $("#signout-container").show();
   // $("#signout-button").hide();
 
@@ -50,17 +49,17 @@ function onSignIn(googleUser) {
   $("#loggedUserImage").attr("src", profile.getImageUrl());
   $("#loggedUsername").html(profile.getName());
   $("#loggedUserEmail").html(profile.getEmail());
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function () {
-	  console.log('User signed out.');
-	  $("#signout-container").hide();
-	  $("#signin-container").show();
-	});
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(() => {
+    console.log("User signed out.");
+    $("#signout-container").hide();
+    $("#signin-container").show();
+  });
 }
